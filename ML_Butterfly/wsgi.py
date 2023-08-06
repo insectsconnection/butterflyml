2628ae688f63b266ce0349322a08f611d84b1242
insectsconnection <102572454+insectsconnection@users.noreply.github.com>
2023-08-07 01:28:31 +0800

    Update wsgi.py
    removed dev

--- ML_Butterfly/wsgi.py
+++ ML_Butterfly/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ML_Butterfly.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ML_Butterfly.settings')
 
 application = get_wsgi_application()
